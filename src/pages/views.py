8dac90604aa7d0a62880529982cdb1b717f97af3
Luis Renan Pavanello <102007776+luisrpavanelli@users.noreply.github.com>
2024-01-18 17:38:42 -0300

    Django Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,14 +5,17 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs): #*args, **kwargs
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>") # string of HTML code
-    
+    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs): 
-    return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request, *args, **kwargs): 
+    return render(request, "contact.html", {})
+    #return HttpResponse("<h1>Contact Page</h1>")
 
-def about_view(*args, **kwargs): 
-    return HttpResponse("<h1>About Page</h1>")
+def about_view(request, *args, **kwargs): 
+    return render(request, "about.html", {})
+    #return HttpResponse("<h1>About Page</h1>")
 
-def social_view(*args, **kwargs): 
-    return HttpResponse("<h1>Social Page</h1>")
+def social_view(request, *args, **kwargs):
+    return render(request, "social.html", {}) 
+    #return HttpResponse("<h1>Social Page</h1>")
--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
