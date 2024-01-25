eb667cf2142505945da10cc563a1cb36898c1ffe
Luis Renan Pavanello <102007776+luisrpavanelli@users.noreply.github.com>
2024-01-25 08:39:09 -0300

    Form Validation Methods

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,18 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title       = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your Title"}))
+    email       = forms.EmailField()
+    description = forms.CharField(
+        required=False, 
+        widget=forms.Textarea(
+            attrs={"class": "new-class-name",
+                   "placeholder": "Your Description",    
+                   "id": "my-id-for-textarea", 
+                   "row": 20, 
+                   "cols":120
+                }))
+    price       = forms.DecimalField(initial=200)  
     class Meta:
         model = Product
         fields = [
@@ -10,8 +22,19 @@ class ProductForm(forms.ModelForm):
             'description',
             'price',
         ]
-
-
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "LRP" in title:
+            return title
+        else:
+            raise forms.ValidationError("This is not a valid title")
+        
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("@gmail.com"):
+            raise forms.ValidationError("This is not a valid email")        
+        return email
+    
 class RawProductForm(forms.Form):
     title       = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your Title"})) 
     description = forms.CharField(
